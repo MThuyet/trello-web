025758051bcf53144b44e8327f7740cf7a708428
MThuyet <mthuyet294@gmail.com>
2025-04-11 09:41:10 +0700

    init project base

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import { Button } from '@mui/material'
+import AccessAlarmIcon from '@mui/icons-material/AccessAlarm'
+import ThreeDRotation from '@mui/icons-material/ThreeDRotation'
+import HomeIcon from '@mui/icons-material/Home'
+import { pink } from '@mui/material/colors'
+
+function App() {
+  return (
+    <>
+      <div>MThuyetDev</div>
+      <Button variant="text">Text</Button>
+      <Button variant="contained">Contained</Button>
+      <Button variant="outlined">Outlined</Button>
+
+      <br />
+      <AccessAlarmIcon />
+      <ThreeDRotation />
+
+      <br />
+      <HomeIcon color="primary" />
+      <HomeIcon color="secondary" />
+      <HomeIcon color="success" />
+      <HomeIcon color="action" />
+      <HomeIcon color="disabled" />
+      <HomeIcon sx={{ color: pink[500] }} />
+    </>
+  )
+}
+
+export default App
