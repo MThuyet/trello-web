77386e788ea37ecb2ef7c802d90ef12fe458db01
MThuyet <mthuyet294@gmail.com>
2025-04-11 10:43:06 +0700

    config and use examp MUI

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import AccessAlarmIcon from '@mui/icons-material/AccessAlarm'
 import ThreeDRotation from '@mui/icons-material/ThreeDRotation'
 import HomeIcon from '@mui/icons-material/Home'
 import { pink } from '@mui/material/colors'
+import Typography from '@mui/material/Typography'
 
 function App() {
   return (
@@ -11,18 +12,21 @@ function App() {
       <Button variant="text">Text</Button>
       <Button variant="contained">Contained</Button>
       <Button variant="outlined">Outlined</Button>
-
       <br />
       <AccessAlarmIcon />
       <ThreeDRotation />
-
-      <br />
       <HomeIcon color="primary" />
       <HomeIcon color="secondary" />
       <HomeIcon color="success" />
       <HomeIcon color="action" />
       <HomeIcon color="disabled" />
       <HomeIcon sx={{ color: pink[500] }} />
+
+      <div>
+        <Typography variant="body2" color={'text.secondary'}>
+          mthuyet
+        </Typography>
+      </div>
     </>
   )
 }
