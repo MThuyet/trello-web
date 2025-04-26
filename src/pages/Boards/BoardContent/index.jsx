2378b4c78682f48ce853a4a60fe39a8149639676
MThuyet <mthuyet294@gmail.com>
2025-04-27 00:45:20 +0700

    refactor: optimize project structure

---
+++ src/pages/Boards/BoardContent/index.jsx
@@ -0,0 +1,18 @@
+import Box from '@mui/material/Box'
+
+const BoardContent = () => {
+  return (
+    <Box
+      sx={{
+        backgroundColor: 'primary.main',
+        widows: '100%',
+        height: (them) => `calc(100vh - ${them.trello.appBarHeight} - ${them.trello.boardBarHeight})`,
+        display: 'flex',
+        alignItems: 'center'
+      }}>
+      Board Content
+    </Box>
+  )
+}
+
+export default BoardContent
