b4ec442b40d87bf37619dc052d0ae3720a8b0197
MThuyet <mthuyet294@gmail.com>
2025-05-03 13:40:44 +0700

    feat: drag_drop_columns

--- src/pages/Boards/BoardContent/BoardContent.jsx
+++ src/pages/Boards/BoardContent/BoardContent.jsx
@@ -1,20 +1,77 @@
 import Box from '@mui/material/Box'
 import ListColumns from './ListColumns/ListColumns'
 import { mapOrder } from '~/utils/sorts'
+import { useEffect, useState } from 'react'
+import { DndContext, useSensor, useSensors, MouseSensor, TouchSensor } from '@dnd-kit/core'
+import { arrayMove } from '@dnd-kit/sortable'
 
 const BoardContent = ({ board }) => {
-  const orderedColumn = mapOrder(board?.columns, board?.columnOrderIds, '_id')
+  // https://docs.dndkit.com/api-documentation/sensors#usesensors
+  // fix khi click cũng kích hoạt event nhưng còn bug
+  // const pointerSensor = useSensor(PointerSensor, {
+  //   activationConstraint: {
+  //     distance: 10 // khoảng cách di chuyển tối thiểu 10px để kích hoạt kéo thả
+  //   }
+  // })
+
+  // ưu tiên sử dụng mouseSensor và toachSensor để có trải nghiệm trên mobile tốt hơn
+
+  const mouseSensor = useSensor(MouseSensor, {
+    activationConstraint: {
+      distance: 10
+    }
+  })
+
+  const toachSensor = useSensor(TouchSensor, {
+    activationConstraint: {
+      delay: 250, // nhấn giữ khoảng 250ms để kích hoạt kéo thả
+      tolerance: 500
+    }
+  })
+
+  const sensors = useSensors(mouseSensor, toachSensor)
+
+  const [orderedColumns, setOrderedColumns] = useState([])
+
+  useEffect(() => {
+    const orderedColumn = mapOrder(board?.columns, board?.columnOrderIds, '_id')
+    setOrderedColumns(orderedColumn)
+  }, [board])
+
+  const handleDragEnd = (event) => {
+    const { active, over } = event
+
+    if (!over) return // nếu kéo đến vị trí không xác định
+
+    // nếu vị trí sau khi kéo khác với vị trí ban đầu -> action
+    if (active.id !== over.id) {
+      // lấy vị trí cũ từ active
+      const oldIndex = orderedColumns.findIndex((c) => c._id === active.id)
+      // lấy vị trí mới từ over
+      const newIndex = orderedColumns.findIndex((c) => c._id === over.id)
+
+      // dùng arrayMove để sắp xếp lại vị trí mảng như đã kéo thả
+      // https://docs.dndkit.com/presets/sortable#overview
+      const dndOrderedColumns = arrayMove(orderedColumns, oldIndex, newIndex)
+      setOrderedColumns(dndOrderedColumns)
+
+      // const dndOrderedColumnsIds = dndOrderedColumns.map((column) => column._id)
+      // console.log('dndOrderedColumnsIds', dndOrderedColumnsIds)
+    }
+  }
 
   return (
-    <Box
-      sx={{
-        bgcolor: (theme) => (theme.palette.mode === 'dark' ? '#34495e' : '#1976d2'),
-        widows: '100%',
-        height: (theme) => theme.trello.boardContentHeight,
-        p: '10px 0'
-      }}>
-      <ListColumns columns={orderedColumn} />
-    </Box>
+    <DndContext onDragEnd={handleDragEnd} sensors={sensors}>
+      <Box
+        sx={{
+          bgcolor: (theme) => (theme.palette.mode === 'dark' ? '#34495e' : '#1976d2'),
+          widows: '100%',
+          height: (theme) => theme.trello.boardContentHeight,
+          p: '10px 0'
+        }}>
+        <ListColumns columns={orderedColumns} />
+      </Box>
+    </DndContext>
   )
 }
 
