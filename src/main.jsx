6f5bdc09015b7ed26401cd45b749afbd24a4df36
MThuyet <mthuyet294@gmail.com>
2025-05-27 22:53:33 +0700

    config: init redux persist & handle redirec using ProtectedRoute

--- src/main.jsx
+++ src/main.jsx
@@ -14,24 +14,31 @@ import { Provider } from 'react-redux'
 import { store } from './redux/store'
 // react router dom
 import { BrowserRouter } from 'react-router-dom'
+// redux persist
+import { PersistGate } from 'redux-persist/integration/react'
+import { persistStore } from 'redux-persist'
+
+let persistor = persistStore(store)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   // basename: đường dẫn gốc của project, luôn luôn phải đi qua trước khi truy cập vào các route khác
   <BrowserRouter basename="/">
     <Provider store={store}>
-      <CssVarsProvider theme={theme}>
-        <ConfirmProvider
-          defaultOptions={{
-            dialogProps: { maxWidth: 'xs' },
-            cancellationButtonProps: { color: 'inherit' },
-            confirmationButtonProps: { variant: 'outlined' },
-            allowClose: false
-          }}>
-          <CssBaseline />
-          <App />
-          <ToastContainer position="bottom-left" theme="colored" limit={3} newestOnTop={true} autoClose={3000} />
-        </ConfirmProvider>
-      </CssVarsProvider>
+      <PersistGate loading={null} persistor={persistor}>
+        <CssVarsProvider theme={theme}>
+          <ConfirmProvider
+            defaultOptions={{
+              dialogProps: { maxWidth: 'xs' },
+              cancellationButtonProps: { color: 'inherit' },
+              confirmationButtonProps: { variant: 'outlined' },
+              allowClose: false
+            }}>
+            <CssBaseline />
+            <App />
+            <ToastContainer position="bottom-left" theme="colored" limit={3} newestOnTop={true} autoClose={3000} />
+          </ConfirmProvider>
+        </CssVarsProvider>
+      </PersistGate>
     </Provider>
   </BrowserRouter>
 )
