b2b6f1a50c5605e562025a011fad357a82c0bc87
MThuyet <mthuyet294@gmail.com>
2025-05-27 12:52:39 +0700

    feat: create userSlice redux & login using toast promise

---
+++ src/redux/user/userSlice.js
@@ -0,0 +1,32 @@
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit'
+import authorizedAxiosInstance from '~/utils/authorizeAxios'
+import { API_ROUTE } from '~/utils/constants'
+
+const initialState = {
+  currentUser: null
+}
+
+export const loginUserAPI = createAsyncThunk('user/loginUserAPI', async (data) => {
+  const response = await authorizedAxiosInstance.post(`${API_ROUTE}/v1/users/login`, data)
+  return response.data
+})
+
+export const userSlice = createSlice({
+  name: 'user',
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder.addCase(loginUserAPI.fulfilled, (state, action) => {
+      const user = action.payload
+      state.currentUser = user
+    })
+  }
+})
+
+// export const {} = userSlice.actions
+
+export const selectCurrentUser = (state) => {
+  return state.user.currentUser
+}
+
+export const userReducer = userSlice.reducer
